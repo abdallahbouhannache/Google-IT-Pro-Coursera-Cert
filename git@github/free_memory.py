48fbb85c994780e72f66b5c25a773fff9f3b0534
abdallah <abdallah.bouhannache@gmail.com>
2023-05-07 20:32:36 +0100

    adding README.MD

--- git@github/free_memory.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-
-def main():
-        """ you need to check disc space """
-        print("Everything is Going Good after checking in!")
-
-
-main()
--- python-for-os-systems/bash script/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if test -n $PATH; then echo "YOUR PATH IS NOT EMPTY"; fi
-
-## test == [] just an alias
-
-if [ -n $PATH ]; then echo "CONFIRMATION OF FIRST PHRASE"; fi
